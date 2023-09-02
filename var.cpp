b012440a58427c7103977aa451bb78cef9bbed03
Abdelaziz Elsawy <abdelaziz38elsawy@gmail.com>
2023-09-02 15:48:31 +0300

    hero newfiles

---
+++ var.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your One Line Code Here
+int a;
+int b;
+int c;
+  // Do Not Edit Below
+  a = 10, b = 20, c = 70;
+  cout << "Sum Of All Number Is: " << a + b + c;
+  return 0;
+}
+
+/*
+Not Valid 
+
+
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [Valid ] => Reason ?
+  __name      [ ??? ] => Reason ?
+  name@name   [ ??? ] => Reason ?
+  name10name  [ ??? ] => Reason ?
+  name!name   [ ??? ] => Reason ?
+  first_NAME  [ ??? ] => Reason ?
+  first_name  [ ??? ] => Reason ?
+  firstName   [ ??? ] => Reason ?
+  first name  [ ??? ] => Reason ?
+  fn          [ ??? ] => Reason ?
+  public      [ ??? ] => Reason ?
+  Public      [ ??? ] => Reason ?
+*/
